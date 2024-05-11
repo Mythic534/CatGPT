fb3e64ac502a9d3e7a8af532d593fe4fedd45bcd
Mythic534 <tom.simpson733@gmail.com>
2024-05-11 14:23:08 +0100

    Bot 1.0

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+import logging
+
+from dotenv import dotenv_values
+from catbot import CatBot, Catifier
+
+# Setup logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+if __name__ == "__main__":
+    config = dotenv_values()
+    ai = Catifier(config)
+    bot = CatBot(config, ai)
+    bot.run()
